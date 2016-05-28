13c9d2319794d4113e00561d82fcf3daf6bba76f
dallascard <dcard@andrew.cmu.edu>
2016-05-27 19:13:47 -0700

    applied log math to other models

--- src/ERLDASampler.java
+++ src/ERLDASampler.java
@@ -438,7 +438,7 @@ class ERLDASampler {
                 // compute probabilities
                 double p_sum = 0;
                 for (int k = 0; k < n_topics; k++) {
-                    pr[k] = (persona_role_topic_counts[p_j][r_j][k] + beta) * (topic_vocab_counts[k][v_j] + gamma) / (topic_tuple_counts[k] + gamma * vocab_size);
+                    pr[k] = Math.exp(Math.log(persona_role_topic_counts[p_j][r_j][k] + beta) + Math.log(topic_vocab_counts[k][v_j] + gamma) - Math.log(topic_tuple_counts[k] + gamma * vocab_size));
                     //pr[k] = (topic_vocab_counts[k][v_j] + gamma) / (topic_tuple_counts[k] + gamma * vocab_size);
                     assert pr[k] > 0;
                     p_sum += pr[k];
