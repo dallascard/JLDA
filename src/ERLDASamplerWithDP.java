622f5d632c33663c3629d9416667eda82130094c
dallascard <dcard@andrew.cmu.edu>
2016-07-11 14:00:58 -0700

    debug

--- src/ERLDASamplerWithDP.java
+++ src/ERLDASamplerWithDP.java
@@ -565,9 +565,9 @@ class ERLDASamplerWithDP {
                 for (int p=0; p < n_personas; p++) {
                     pr[p] = Math.log(story_type_persona_counts[s_e][p] + alpha);
                 }
+                System.out.println(e);
                 List<Integer> tuples = entity_tuples.get(e);
                 assert tuples != null;
-                System.out.println(tuples);
                 for (int t : tuples) {
                     int topic_t = tuple_topics[t];
                     int role_t = tuple_role[t];
