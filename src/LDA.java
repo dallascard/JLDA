b0b31e079087de52da3c7a1c61fc67493a342939
dallascard <dcard@andrew.cmu.edu>
2017-01-11 16:37:50 -0800

    refactored to supress unchecked warning in adding to json object

--- src/LDA.java
+++ src/LDA.java
@@ -51,20 +51,27 @@ public class LDA {
         int burn_in = Integer.parseInt(params.get("-u"));
         int subsampling = Integer.parseInt(params.get("-s"));
 
-        LDASampler LDASampler = new LDASampler(word_num_file, word_doc_file, vocab_file);
-        int word_topic_matrix[][] = LDASampler.run(n_topics, alpha, beta, n_iter, burn_in, subsampling);
+        LDASampler sampler = new LDASampler(word_num_file, word_doc_file, vocab_file);
+        int word_topic_matrix[][] = sampler.run(n_topics, alpha, beta, n_iter, burn_in, subsampling);
         int vocab_size = (int) word_topic_matrix.length;
         System.out.println(vocab_size);
 
-        String vocab[] = LDASampler.get_vocab();
+        outputSamples(n_topics, vocab_size, params.get("-o"), sampler, word_topic_matrix);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    static void outputSamples(int n_topics, int vocab_size, String outputDir, LDASampler sampler, int[][] word_topic_matrix)  throws Exception {
+
+        String vocab[] = sampler.get_vocab();
 
         System.out.println("Writing results to file");
         for (int k=0; k < n_topics; k++) {
-            Path output_file = Paths.get(params.get("-o"), k + ".json");
+            Path output_file = Paths.get(outputDir, k + ".json");
             JSONObject obj = new JSONObject();
 
             for (int v=0; v < vocab_size; v++)
-                obj.put(new String(vocab[v].getBytes("UTF-8"), "UTF-8"), word_topic_matrix[v][k]);
+                obj.put(new String(vocab[v].getBytes("UTF-8"), "UTF-8"),  word_topic_matrix[v][k]);
 
             try (FileWriter file = new FileWriter(output_file.toString())) {
                 file.write(obj.toJSONString());
